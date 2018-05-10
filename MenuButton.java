81d552bd3f5b309fcb7ee9baf09fcc2a76a9c3b6
MikeT666 <38770716+MikeT666@users.noreply.github.com>
2018-05-10 10:13:25 +1000

    Create MenuButton.java
    JButton supports Menu.java

---
+++ MenuButton.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+//JButton for Menu and can be customized further if needed
+public class MenuButton extends JButton{
+
+    public MenuButton(String name) {
+        super(name);
+    }
+}
